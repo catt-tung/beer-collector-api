445fda13dd04fda87155421c1594df68cda7a51e
Catherine Tung <catt.tung@gmail.com>
2022-04-26 13:05:47 -0400

    Shop model

---
+++ api/models/shop.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+from api.models.db import db
+
+class Shop(db.Model):
+    __tablename__ = 'toys'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    location = db.Column(db.String(250))
+    vibe = db.Column(db.String(100))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    profile_id = db.Column(db.Integer, db.ForeignKey('profiles.id'))
+
+    def __repr__(self):
+      return f"Shop('{self.id}', '{self.name}'"
+
+    def serialize(self):
+      shop = {c.name: getattr(self, c.name) for c in self.__table__.columns}
+      return shop
