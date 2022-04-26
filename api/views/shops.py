5065ea2769922eb8591d320876f0e347c31bbac8
Catherine Tung <catt.tung@gmail.com>
2022-04-26 13:20:22 -0400

    Create a shop

--- api/views/shops.py
+++ api/views/shops.py
@@ -4,4 +4,17 @@ from api.middleware import login_required, read_token
 from api.models.db import db
 from api.models.shop import Shop
 
-toys = Blueprint('shops', 'shops')
+shops = Blueprint('shops', 'shops')
+
+# create a toy
+@shops.route('/', methods=["POST"]) 
+@login_required
+def create():
+  data = request.get_json()
+  profile = read_token(request)
+  data["profile_id"] = profile["id"]
+
+  shop = Shop(**data)
+  db.session.add(shop)
+  db.session.commit()
+  return jsonify(shop.serialize()), 201
